d2ad85ef30e4ef05726512caafe19cfcf9b04e0f
Mo <10432473+morrieinmaas@users.noreply.github.com>
2022-12-07 11:20:48 +0100

    feat: add abstract base BLE class (#14)
    Signed-off-by: Moriarty <moritz@animo.id>

---
+++ src/ble.ts
@@ -0,0 +1,11 @@
+export interface Ble {
+  sendMessage(message: string): Promise<void>
+  start(
+    serviceUUID: string,
+    writeCharacteristicUUID: string,
+    indicationCharacteristicUUID: string
+  ): Promise<void>
+  //   TODO: implement native shutdown
+  shutdown(): Promise<void>
+  registerMessageListener(cb: (msg: string) => void): void
+}
