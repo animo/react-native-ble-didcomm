cb00dc704bc31bdcd9702355312eb4d97dfadf96
Jim Ezesinachi <jim@animo.id>
2023-02-01 20:37:30 +0100

    fix: removed errant import
    Signed-off-by: Jim Ezesinachi <jim@animo.id>

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -12,8 +12,7 @@ import {
   Peripheral,
   DEFAULT_DIDCOMM_SERVICE_UUID,
   DEFAULT_DIDCOMM_MESSAGE_CHARACTERISTIC_UUID,
-  DEFAULT_DIDCOMM_INDICATE_CHARACTERISTIC_UUID,
-  write
+  DEFAULT_DIDCOMM_INDICATE_CHARACTERISTIC_UUID
 } from '@animo-id/react-native-ble-didcomm'
 import { presentationMsg } from './presentationMsg'
 
