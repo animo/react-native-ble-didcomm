7fc43ada058cae6499343c9e4bd420f9d81cfaea
Jim Ezesinachi <jim@animo.id>
2023-03-20 08:34:47 +0100

    fix: lint fix
    Signed-off-by: Jim Ezesinachi <jim@animo.id>

--- src/ble.ts
+++ src/ble.ts
@@ -11,7 +11,9 @@ export interface Ble {
   start(): Promise<void>
   setService(options: ServiceOptions): Promise<void>
   shutdown(): Promise<void>
-  registerMessageListener(cb: (data: { message: string }) => void): EmitterSubscription
+  registerMessageListener(
+    cb: (data: { message: string }) => void
+  ): EmitterSubscription
   registerOnConnectedListener(
     cb: (options: { identifier: string }) => void
   ): EmitterSubscription
