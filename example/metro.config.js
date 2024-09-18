06fa34707bcbc3db0e35414c1dd3fcc549fc4493
Berend Sliedrecht <sliedrecht@berend.io>
2024-09-18 16:00:04 +0200

    tmp
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,3 +1,5 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
 const path = require('node:path')
 const esc = require('escape-string-regexp')
 const exclusionList = require('metro-config/src/defaults/exclusionList')
@@ -9,7 +11,7 @@ const modules = Object.keys({
   ...pak.peerDependencies,
 })
 
-module.exports = {
+const config = {
   projectRoot: __dirname,
   watchFolders: [root],
 
@@ -30,4 +32,7 @@ module.exports = {
       },
     }),
   },
-}
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
